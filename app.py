12445bab71404ac2a5dd21957dd9a1f6d25d7d89
Rubin  Ramallari <rubinramallari9@gmail.com>
2026-06-17 11:31:11 +0200

    ...

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import Flask, request, render_template, jsonify
-import sqlite3
+import pyodbc
+import configparser
+from dotenv import load_dotenv
 import os
 
-DB_PATH = os.path.join(os.path.dirname(__file__), 'dummy.db')
+load_dotenv()
 
 app = Flask(__name__)
 
 def get_db_connection():
-    connection = sqlite3.connect(DB_PATH)
-    connection.row_factory = sqlite3.Row
+    config = configparser.ConfigParser()
+    config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
+    db_config = config['database']
+
+    connection = pyodbc.connect(
+        f"DRIVER={db_config['driver']};"
+        f"SERVER={db_config['server']};"
+        f"DATABASE={db_config['database']};"
+        f"UID={os.getenv('USER')};"
+        f"PWD={os.getenv('PASSWORD')};"
+    )
     return connection
 
 @app.route('/')
@@ -21,21 +32,16 @@ def get_requestor_name():
     ReportTitle = request.args.get('ReportTitle')
     connection = get_db_connection()
     cursor = connection.cursor()
-    
-    
+
     if RequestorDepartment and ReportTitle:
-        query = "SELECT DISTINCT RequestorName FROM ReportList WHERE RequestorDepartment=? AND ReportTitle=?"
-        cursor.execute(query, (RequestorDepartment, ReportTitle))
+        cursor.execute("SELECT DISTINCT RequestorName FROM BI_REPORT_MONITOR.dbo.ReportList WHERE RequestorDepartment=? AND ReportTitle=?", (RequestorDepartment, ReportTitle))
     elif RequestorDepartment:
-        query = "SELECT DISTINCT RequestorName FROM ReportList WHERE RequestorDepartment=?"
-        cursor.execute(query, (RequestorDepartment,))
+        cursor.execute("SELECT DISTINCT RequestorName FROM BI_REPORT_MONITOR.dbo.ReportList WHERE RequestorDepartment=?", (RequestorDepartment,))
     elif ReportTitle:
-        query = "SELECT DISTINCT RequestorName FROM ReportList WHERE ReportTitle=?"
-        cursor.execute(query, (ReportTitle,))
+        cursor.execute("SELECT DISTINCT RequestorName FROM BI_REPORT_MONITOR.dbo.ReportList WHERE ReportTitle=?", (ReportTitle,))
     else:
-        query = "SELECT DISTINCT RequestorName FROM ReportList"
-        cursor.execute(query)
-    
+        cursor.execute("SELECT DISTINCT RequestorName FROM BI_REPORT_MONITOR.dbo.ReportList")
+
     names = [row[0] for row in cursor.fetchall()]
     connection.close()
     return jsonify(names)
@@ -44,23 +50,18 @@ def get_requestor_name():
 def get_requestor_department():
     RequestorName = request.args.get('RequestorName')
     ReportTitle = request.args.get('ReportTitle')
-    
     connection = get_db_connection()
     cursor = connection.cursor()
-    
+
     if RequestorName and ReportTitle:
-        query = "SELECT DISTINCT RequestorDepartment FROM ReportList WHERE RequestorName=? AND ReportTitle=?"
-        cursor.execute(query, (RequestorName, ReportTitle))
+        cursor.execute("SELECT DISTINCT RequestorDepartment FROM BI_REPORT_MONITOR.dbo.ReportList WHERE RequestorName=? AND ReportTitle=?", (RequestorName, ReportTitle))
     elif RequestorName:
-        query = "SELECT DISTINCT RequestorDepartment FROM ReportList WHERE RequestorName=?"
-        cursor.execute(query, (RequestorName,))
+        cursor.execute("SELECT DISTINCT RequestorDepartment FROM BI_REPORT_MONITOR.dbo.ReportList WHERE RequestorName=?", (RequestorName,))
     elif ReportTitle:
-        query = "SELECT DISTINCT RequestorDepartment FROM ReportList WHERE ReportTitle=?"
-        cursor.execute(query, (ReportTitle,))
+        cursor.execute("SELECT DISTINCT RequestorDepartment FROM BI_REPORT_MONITOR.dbo.ReportList WHERE ReportTitle=?", (ReportTitle,))
     else:
-        query = "SELECT DISTINCT RequestorDepartment FROM ReportList"
-        cursor.execute(query)
-    
+        cursor.execute("SELECT DISTINCT RequestorDepartment FROM BI_REPORT_MONITOR.dbo.ReportList")
+
     departments = [row[0] for row in cursor.fetchall()]
     connection.close()
     return jsonify(departments)
@@ -71,25 +72,19 @@ def get_report_title():
     RequestorDepartment = request.args.get('RequestorDepartment')
     connection = get_db_connection()
     cursor = connection.cursor()
-    
+
     if RequestorName and RequestorDepartment:
-        query = "SELECT DISTINCT ReportTitle FROM ReportList WHERE RequestorName=? AND RequestorDepartment=?"
-        cursor.execute(query, (RequestorName, RequestorDepartment))
+        cursor.execute("SELECT DISTINCT ReportTitle FROM BI_REPORT_MONITOR.dbo.ReportList WHERE RequestorName=? AND RequestorDepartment=?", (RequestorName, RequestorDepartment))
     elif RequestorName:
-        query = "SELECT DISTINCT ReportTitle FROM ReportList WHERE RequestorName=?"
-        cursor.execute(query, (RequestorName,))
+        cursor.execute("SELECT DISTINCT ReportTitle FROM BI_REPORT_MONITOR.dbo.ReportList WHERE RequestorName=?", (RequestorName,))
     elif RequestorDepartment:
-        query = "SELECT DISTINCT ReportTitle FROM ReportList WHERE RequestorDepartment=?"
-        cursor.execute(query, (RequestorDepartment,))
+        cursor.execute("SELECT DISTINCT ReportTitle FROM BI_REPORT_MONITOR.dbo.ReportList WHERE RequestorDepartment=?", (RequestorDepartment,))
     else:
-        query = "SELECT DISTINCT ReportTitle FROM ReportList"
-        cursor.execute(query)
-    
+        cursor.execute("SELECT DISTINCT ReportTitle FROM BI_REPORT_MONITOR.dbo.ReportList")
+
     titles = [row[0] for row in cursor.fetchall()]
     connection.close()
     return jsonify(titles)
-
-
 
 @app.route('/search', methods=['GET'])
 def search():
@@ -98,9 +93,10 @@ def search():
     ReportTitle = request.args.get('ReportTitle')
     connection = get_db_connection()
     cursor = connection.cursor()
-    query = """SELECT * FROM ReportList
-               WHERE RequestorName=? AND RequestorDepartment=? AND ReportTitle=?"""
-    cursor.execute(query, (RequestorName, RequestorDepartment, ReportTitle))
+    cursor.execute(
+        "SELECT * FROM BI_REPORT_MONITOR.dbo.ReportList WHERE RequestorName=? AND RequestorDepartment=? AND ReportTitle=?",
+        (RequestorName, RequestorDepartment, ReportTitle)
+    )
     columns = [column[0] for column in cursor.description]
     results = [dict(zip(columns, row)) for row in cursor.fetchall()]
     connection.close()
@@ -108,4 +104,3 @@ def search():
 
 if __name__ == "__main__":
     app.run(debug=True, port=8088)
-
